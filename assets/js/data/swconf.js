df91af152aae260a5eac10b3dd3e4434e2e51b46
Arth2001 <Arth2001@users.noreply.github.com>
2025-02-23 23:52:04 +0000

    deploy: a61d8f8871b737741db794a62e864fe558babb2a

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740354710',resources: [
+    cacheName: 'chirpy-1740354723',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
